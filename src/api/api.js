0430ed074133d5b5b0faf2fac1991ba6cbf79eba
minhttran <trcongminh.261002@gmail.com>
2024-06-23 22:46:23 +0700

    Admin login setup with api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5500",
+    baseURL: "http://localhost:5500/api/",
 });
 
 export default api;
--- src/redux/Reducers/authReducer.js
+++ src/redux/Reducers/authReducer.js
@@ -4,13 +4,12 @@ import api from "../../api/api";
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
     async (info) => {
-        console.log(info)
         try {
-            // const data = api.post('/admin/login', info,
-            //     { withCredentials: true });
-            // console.log(data)
+            const { data } = await api.post('admin/login', info,
+                { withCredentials: true });
+            console.log(data)
         } catch (error) {
-
+            console.log(error.message);
         }
     }
 )
